38ee1fe99545deeb09f768fdb31b8dd62779b1a3
Christian Bay <christian.bay@studium.fau.de>
2015-10-21 16:42:45 +0200

    Flags and strtol

--- 0/Jacobi.c
+++ 0/Jacobi.c
@@ -1,26 +1,36 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <errno.h>
+#include <limits.h>
 
 
 #define GRID_T float
 
+/* Variables */
 static GRID_T *old_grid, *new_grid;
-
 static int width, height;
 
+/* Method declarations */
 static void init(GRID_T *);
+static long help_strtol(char *);
 
 int main(int argc, char** argv){
 
-	width  = argv[1];
-	height = argv[2];
+	if(argc < 3){
+		fprintf(stderr, "Too less arguments: Jacobi [width] [height]\n");
+		exit(EXIT_FAILURE);
+	}
 
-	errno = posix_memalign(&old_grid, 64, width*height);
+	width  = help_strtol(argv[1]);
+	height = help_strtol(argv[2]);
+
+
+	errno = posix_memalign((void **) &old_grid, 64, width*height);
 	if(errno){
 		perror("posix_memalign");
 	}
 
-	errno = posix_memalign(&new_grid, 64, width*height);
+	errno = posix_memalign((void **) &new_grid, 64, width*height);
 	if(errno){
 		perror("posix_memalign");
 	}
@@ -30,12 +40,36 @@ int main(int argc, char** argv){
 	init(old_grid);
 	init(new_grid);
 
-	free(&old_grid);
-	free(&new_grid);
+	free(old_grid);
+	free(new_grid);
 	return 0;
 }
 
 /* Method for initialization of a grid */
 static void init(GRID_T *grid){
+	// TODO
 	return;
+}
+
+/* Convert string to long */
+static long help_strtol(char *string){
+	errno = 0;
+	long val;
+	char *endptr;
+	val = strtol(string, &endptr, 10);
+
+	/* Check for various possible errors */
+
+	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+			|| (errno != 0 && val == 0)) {
+		perror("strtol");
+		exit(EXIT_FAILURE);
+	}
+
+	if (endptr == string) {
+		fprintf(stderr, "No digits were found\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return val;
 }
