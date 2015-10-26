db7418263634d75bf227448b4fa89a2233a2cab8
Christian Bay <christian.bay@studium.fau.de>
2015-10-26 12:47:46 +0100

    Reduce vanilla code for simplification of assembly code

--- 0/JacobiVanilla.c
+++ 0/JacobiVanilla.c
@@ -5,14 +5,9 @@
 #include "Jacobi.h"
 
 void jacobiVanilla(GRID_T *oldGrid, GRID_T *newGrid, int width, int height){
-	GRID_T left, right, down, up;
 	for(int i = 1; i < height-1; i++){
 		for(int j = 1; j < width-1; j++){
-			left = oldGrid[i*width + (j-1)];
-			right = oldGrid[i*width + (j+1)];
-			up = oldGrid[(i-1)*width + j];
-			down = oldGrid[(i+1)*width + j];
-			newGrid[i*width + j] = (left + right + down + up) * 0.25;
+			newGrid[i*width + j] = (oldGrid[i*width + (j-1)] + oldGrid[i*width + (j+1)] + oldGrid[(i-1)*width + j] + oldGrid[(i+1)*width + j]) * 0.25;
 		}
 	}
 	return;
