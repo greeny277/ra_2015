5c059a73a2fc7bb8d39e2e9a7b93991a1bcb418c
Christian Bay <christian.bay@studium.fau.de>
2015-10-30 12:41:33 +0100

    nums as haskell script (just for fun)

---
+++ 1/nums.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-} 
+import Turtle
+import qualified Control.Foldl as Fold
+
+main :: IO ()
+main = fold computation Fold.nub >>= mapM_ (print . format (""%s%""))
+
+computation :: Shell Text
+computation = do
+        x <- select [1..68]
+        let point = repr (getMeasurePoint x)
+        return $ head $ cut (char '.') point
+
+getMeasurePoint :: Int -> Double
+getMeasurePoint n = exp (logBase (exp 1) (128*1024)/68)^n
