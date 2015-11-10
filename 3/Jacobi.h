84dd488ed398e2421141c715c3465cbc2b88c76c
Christian Bay <christian.bay@studium.fau.de>
2015-11-10 15:30:33 +0100

    Ex3: Implement SSE version. Not very fast imho

---
+++ 3/Jacobi.h
@@ -0,0 +1,14 @@
+/*
+ * GRID_T is the datatype of the grid entries
+ */
+#define GRID_T float
+
+/*
+ * How many seconds after loop stops
+ */
+#define LOOP_TIME 1
+/*
+ * Choose SP or DP
+ */
+#define FLOAT
+//#define DOUBLE
---
+++ 3/JacobiVanilla.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <immintrin.h>
+
+#include "Jacobi.h"
+
+static void jacobi_avx(GRID_T *, GRID_T *, int, int);
+static void jacobi_normal(GRID_T *, GRID_T *, int, int);
+static void jacobi_sse(GRID_T *, GRID_T *, int, int);
+
+void jacobiVanilla(GRID_T *oldGrid, GRID_T *newGrid, int width, int height){
+#ifdef FLOAT
+	jacobi_sse(oldGrid, newGrid, width, height);
+	return;
+#endif
+
+#ifdef DOUBLE
+	jacobi_avx(oldGrid, newGrid, width, height);
+	return;
+#endif
+
+	jacobi_normal(oldGrid, newGrid, width, height);
+	return;
+}
+
+static void jacobi_sse(GRID_T *oldGrid, GRID_T *newGrid, int width, int height){
+	/* Contains all 4 upper values */
+	__m128 up_row, below_row, right_row, left_row;
+
+	__m128 factor = _mm_set_ps(0.25f, 0.25f, 0.25f, 0.25f);
+
+	for(int i = 1; i < height-1; i++){
+		for(int j = 1; j < width-4; j += 4){
+			up_row = _mm_set_ps(oldGrid[(i-1)*width + j], oldGrid[(i-1)*width + (j+1)], oldGrid[(i-1)*width + (j+2)], oldGrid[(i-1)*width + (j+3)]);
+			below_row = _mm_set_ps(oldGrid[(i+1)*width + j], oldGrid[(i+1)*width + (j+1)], oldGrid[(i+1)*width + (j+2)], oldGrid[(i+1)*width + (j+3)]);
+
+			right_row = _mm_set_ps(oldGrid[i*width + (j+1)], oldGrid[i*width + (j+2)], oldGrid[i*width + (j+3)], oldGrid[i*width + (j+4)]);
+			left_row = _mm_set_ps(oldGrid[i*width + (j-1)], oldGrid[i*width + j], oldGrid[i*width + (j+1)], oldGrid[i*width + (j+2)]);
+
+			/* Sum up n-th element of each vector */
+			__m128 dest;
+			dest =  _mm_add_ps(up_row, below_row);
+			dest =  _mm_add_ps(dest, right_row);
+			dest =  _mm_add_ps(dest, left_row);
+
+			/* Multiplicat with 0.25 */
+			dest = _mm_mul_ps(dest, factor);
+
+			// Use unaligned store method. Normal one produces segmentation fault
+			_mm_storeu_ps(&(newGrid[i*width + j]), dest);
+		}
+	}
+	return;
+}
+
+static void jacobi_normal(GRID_T *oldGrid, GRID_T *newGrid, int width, int height){
+	for(int i = 1; i < height-1; i++){
+		for(int j = 1; j < width-1; j++){
+			newGrid[i*width + j] = (oldGrid[i*width + (j-1)] + oldGrid[i*width + (j+1)] + oldGrid[(i-1)*width + j] + oldGrid[(i+1)*width + j]) * 0.25;
+		}
+	}
+	return;
+}
+
+static void jacobi_avx(GRID_T *oldGrid, GRID_T *newGrid, int width, int height){
+	return;
+}
