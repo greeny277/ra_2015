640ab7230284f35bf7caef3380bca4686798bd1e
Christian Bay <christian.bay@studium.fau.de>
2015-11-10 16:28:06 +0100

    Ex3: Customize plot.sh script for purpose of current exercise

---
+++ 2/plot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+datfile="loops.dat"
+datfile_noext="loops"
+
+
+# create new loops.dat
+printf "# This file is called %s.dat\n" "$datfile_noext" > $datfile
+printf "# Filesize(KiB)  Normal\n" >> $datfile
+while read line
+do
+
+	# get mups
+	length_quad=$(echo "sqrt($line*1024)" | bc -l)
+	length_quad_kiB=$(echo "$length_quad" | bc | sed 's/\..*//g')
+	echo "$length_quad_kiB"
+	mups=$(./Jacobi "$length_quad_kiB" "$length_quad_kiB")
+	printf "  %s         %s\n" "$line" "$mups" >> $datfile
+done < "${1:-/dev/stdin}"
